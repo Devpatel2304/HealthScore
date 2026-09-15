0ff132a950076bb772febbcc605f8e96d39f73f6
Devpatel2304 <149056584+Devpatel2304@users.noreply.github.com>
2026-09-15 16:41:39 +0530

    added main.py file

---
+++ main.py
@@ -0,0 +1,76 @@
+import joblib
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Literal
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+
+class StudentData(BaseModel):
+    age                     : int = Field(..., ge=10, le=100)
+    gender                  : Literal['Male', 'Female']
+    country                 : str
+    academic_level          : Literal['Undergraduate', 'Graduate', 'High School']
+    most_used_platform      : Literal['Facebook', 'LinkedIn', 'Instagram', 'Snapchat','Twitter','YouTube', 'TikTok', 'LINE', 'KakaoTalk', 'VKontakte', 'WhatsApp','WeChat']
+    purpose_of_use          : Literal['Networking', 'Education', 'Entertainment', 'News']
+    avg_daily_usage_hours   : float = Field(..., ge=0, le=24)
+    daily_unlocks           : int   = Field(..., ge=0)
+    study_hours             : float = Field(..., ge=0, le=24)
+    physical_activity_hours : float = Field(..., ge=0, le=24)
+    sleep_hours_per_night   : float = Field(..., ge=0, le=24)
+    stress_level            : Literal['Medium', 'Low', 'Very High', 'High']
+
+# Describe what we send back
+class PredictionResponse(BaseModel):
+    predicted_mental_health_score:float
+    #6.777777 -> float
+
+
+app = FastAPI()
+BASE_DIR = Path(__file__).resolve().parent
+model = joblib.load(BASE_DIR / "Backend" / "xgboost_tuned_pipeline.pkl")
+
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "Backend" / "Frontend"),
+    name="static"
+)
+top_countries = ['Other','India','USA','Canada','Australia','UK','Germany','Mexico','Turkey','France']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return FileResponse(BASE_DIR / "Backend" / "Frontend" / "index.html")
+
+@app.post("/predict", response_model=PredictionResponse)
+def predict_mental_health(data: StudentData):
+    country_group = data.country if data.country in top_countries else 'Other'
+    input_row = pd.DataFrame([{
+        'Age'                       :data.age,
+        'Gender'                    :data.gender,
+        'Country'                   :data.country,
+        'Academic_Level'            :data.academic_level,
+        'Most_Used_Platform'        :data.most_used_platform,
+        'Purpose_Of_Use'            :data.purpose_of_use,
+        'Avg_Daily_Usage_Hours'     :data.avg_daily_usage_hours,
+        'Daily_Unlocks'             :data.daily_unlocks,
+        'Study_Hours'               :data.study_hours,
+        'Physical_Activity_Hours'   :data.physical_activity_hours,
+        'Sleep_Hours_Per_Night'     :data.sleep_hours_per_night,
+        'Stress_Level'              :data.stress_level,
+        'Grouped_country'           :country_group
+   }])
+
+    prediction = model.predict(input_row)[0]
+    return PredictionResponse(predicted_mental_health_score=round(float(prediction), 2))
